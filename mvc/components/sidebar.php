3dd8388e18dcffa5c0f16875873da9ed351c80f5
Filippo Vicari <fil.vicari@gmail.com>
2021-01-06 00:31:25 +0100

    inserted button to delete a note

--- mvc/components/sidebar.php
+++ mvc/components/sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar">
     <nav class="stack pb-2">
-        <h1 class="side-title"><i class="fas fa-save sepia checkpoints"></i> Checkpoints</h1>
+        <h1 class="side-title"><i class="fas fa-save sepia checkpoints"></i> Checkpoint</h1>
         <div id="notes_container">
 
             <textarea class="mb-1" id="notes"> </textarea>
@@ -10,39 +10,17 @@
         </div>
 
         <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
+            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo<div class="del"><i class="fas fa-times-circle"></i></div></div>
         </div>
-        <div class="cursor-pointer" onclick="goto_note_time(55205)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
+      
+        <div class="cursor-pointer" onclick="goto_note_time(555)">
+            <div class="note-item">Cosa importante 2<div class="del"><i class="fas fa-times-circle"></i></div></div>
         </div>
         <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
+            <div class="note-item">Banane<div class="del"><i class="fas fa-times-circle"></i></div></div>
         </div>
         <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
-        </div>
-        <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
-        </div>
-        <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
-        </div>
-        <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
-        </div>
-        <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 1 che devo sapere per fare quella cosa li che non mi ricordo</div>
-
-        </div>
-
-        <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Cosa importante 2</div>
-        </div>
-        <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Banane</div>
-        </div>
-        <div class="cursor-pointer" onclick="goto_note_time(555)">
-            <div class="note-item">Latte</div>
+            <div class="note-item">Latte<div class="del"><i class="fas fa-times-circle"></i></div></div>
         </div>
     </nav>
 </div>
